483a3623abde7aeaefd9fd8b3f7c9a2b7692c47f
jsw <jsw@jsws-MacBook-Pro.local>
2016-09-25 10:14:41 -0300

    import learn ruby the hard way solutions

---
+++ ruby/lrthw/ex1.rb
@@ -0,0 +1,1 @@
+puts 'I "said" do not touch this.'
---
+++ ruby/lrthw/ex5.rb
@@ -0,0 +1,18 @@
+my_name = 'Zed A. Shaw'
+my_age = 35 # not a lie
+my_height = 74
+my_weight = 180
+my_eyes = 'blue'
+my_teeth = 'white'
+my_hair = 'brown'
+
+puts "Let's talk about %s." % my_name
+puts "He's %d inches tall." % my_height
+puts "He's %d pounds heavy." % my_weight
+puts "Actually that's not too heavy."
+puts "He's got %s eyes and %s hair." % [my_eyes, my_hair]
+puts "His teeth are usually %s depending on the coffee." % my_teeth
+
+puts "if I add %d, %d, and %d I get %d." % [my_age, my_height, my_weight, my_age
+                                            + my_height + my_weight]
+
