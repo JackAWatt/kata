0266242c0de969cc3a8ffa26e31a97e8b5a450a1
jsw <jsw@jsws-MacBook-Pro.local>
2016-09-25 14:15:23 -0300

    clean up

--- Crystal reports/Projects/aemployee/aemployee/Form1.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class Form1
-
-    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
-        'TODO: This line of code loads data into the 'xtremeDataSet.Employee' table. You can move, or remove it, as needed.
-        Me.EmployeeTableAdapter.Fill(Me.xtremeDataSet.Employee)
-        Me.ReportViewer1.RefreshReport()
-    End Sub
-
-End Class
--- Crystal reports/product vendor list/Form1.vb
+++
@@ -1,7 +0,0 @@
-﻿Imports CrystalDecisions.Shared
-Imports CrystalDecisions.CrystalReports.Engine
-Imports CrystalDecisions.Windows.Forms
-
-Public Class Form1
-
-End Class
