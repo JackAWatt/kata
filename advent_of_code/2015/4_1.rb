10befb56566d5d3f6579f76a6f5e2d700345aa56
jsw <jsw@jsws-MacBook-Pro.local>
2016-12-04 22:38:49 -0400

    finish 2015: day 4

---
+++ advent_of_code/2015/4_1.rb
@@ -0,0 +1,9 @@
+require 'openssl'
+input = 'iwrupvqb'
+range = 0..1.0/0
+range.each do |x|
+  md5 = OpenSSL::Digest::MD5.hexdigest(input + x.to_s)
+  if md5 =~ /^00000/
+    p x.to_s << " : " << md5.to_s
+  end
+end
