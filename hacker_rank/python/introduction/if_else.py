f682452a0bf35158ce7350657836352fff46f324
Jack Watt <jsw.watt@gmail.com>
2017-01-07 12:31:39 -0400

    Create if_else.py

---
+++ hacker_rank/python/introduction/if_else.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    n = int(input())
+    
+    if n % 2 == 0 and n > 20:
+        print("Not Weird")
+    elif n % 2 == 0 and n >= 6 and n <= 20:
+        print("Weird")
+    elif n % 2 == 0 and n >= 2 and n <= 5:
+        print("Not Weird")
+    elif n % 2 == 1:
+        print("Weird")
+    else:
+        print("Not Weird")
