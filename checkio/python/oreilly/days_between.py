0da95bdfc184614edca41b41ac3409295352fff6
Jack Watt <jsw.watt@gmail.com>
2017-01-07 00:25:39 -0400

    Update days_between.py

---
+++ checkio/python/oreilly/days_between.py
@@ -0,0 +1,12 @@
+import datetime
+
+def days_diff(date1, date2):
+    d1 = datetime.datetime(*date1)
+    d2 = datetime.datetime(*date2)
+    return abs((d2-d1).days)
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert days_diff((1982, 4, 19), (1982, 4, 22)) == 3
+    assert days_diff((2014, 1, 1), (2014, 8, 27)) == 238
+    assert days_diff((2014, 8, 27), (2014, 1, 1)) == 238
