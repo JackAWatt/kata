dc2d59e57a0a2cd28e2b6f0fd1a4ed8d714a845d
Jack Watt <jsw.watt@gmail.com>
2017-01-06 22:44:15 -0400

    Create hello_world.hs

---
+++ hacker_rank/functional_programming/introduction/hello_world.hs
@@ -0,0 +1,7 @@
+-- Enter your code here. Read input from STDIN. Print output to STDOUT
+world = "Hello World"
+hello_world = putStr world -- Fill up this function
+
+-- This part relates to Input/Output and can be used as it is. Do not modify this section
+main = do
+   hello_world
