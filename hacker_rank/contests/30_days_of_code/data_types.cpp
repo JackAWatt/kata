61d42e519d485ba7c3db8eeafc8bb904f76f6d06
Jack Watt <jsw.watt@gmail.com>
2017-01-06 23:54:40 -0400

    Create data_types.cpp

---
+++ hacker_rank/contests/30_days_of_code/data_types.cpp
@@ -0,0 +1,21 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    cout << "Primitive : double\n"
+         << "Primitive : char\n"
+         << "Primitive : boolean\n"
+         << "Primitive : int\n"
+         << "Referenced : String\n"
+         << "Primitive : boolean\n"
+         << "Primitive : double\n"
+         << "Primitive : char\n"
+         << "Referenced : String";
+    return 0;
+}
