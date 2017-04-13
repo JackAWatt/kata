18390b6eee5058a21d6289ce810444accd238bc6
watt <jack.a.watt@gmail.com>
2017-04-13 19:07:49 -0300

    create projects to practice drawing

---
+++ javascript/test/test.js
@@ -0,0 +1,9 @@
+x = 100;
+function setup() {
+  createCanvas(x,x);
+}
+
+function draw() {
+  backgrond(110);
+  stroke(255);
+}
---
+++ javascript/test_2/test_2.js
@@ -0,0 +1,25 @@
+x = 155;
+//PVector position;
+//position = new PVector(x,x);
+var iMax = x;
+var jMax = x;
+
+
+function setup() {
+  createCanvas(x,x);
+}
+
+function draw() {
+  background(100);
+  stroke(0);
+  strokeWeight(4);
+  line(100,100,50,50);
+  var derp = [[],[]];
+  for (var i = 0; i<x;i++){
+    for (var j = 0; i<x;i++){
+      if (i%4 && j%3){
+        
+      }
+    }
+  }
+}
